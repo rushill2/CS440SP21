ff37d34ba7a0d058be95f533519cc2f0251a95e3
rushill2 <67648243+rushill2@users.noreply.github.com>
2021-04-28 17:13:28 -0500

    Add files via upload

---
+++ RL Model/tabular.py
@@ -0,0 +1,117 @@
+import math
+
+import gym
+import numpy as np
+import torch
+
+import utils
+from policies import QPolicy
+
+
+class TabQPolicy(QPolicy):
+    def __init__(self, env, buckets, actionsize, lr, gamma, model=None):
+        """
+        Inititalize the tabular q policy
+
+        @param env: the gym environment
+        @param buckets: specifies the discretization of the continuous state space for each dimension
+        @param actionsize: dimension of the descrete action space.
+        @param lr: learning rate for the model update 
+        @param gamma: discount factor
+        @param model (optional): Load a saved table of Q-values for each state-action
+            model = np.zeros(self.buckets + (actionsize,))
+            
+        """
+        super().__init__(len(buckets), actionsize, lr, gamma)
+        self.env = env
+        self.buckets = buckets
+        if model is None:
+            self.model = np.zeros(self.buckets + (actionsize,)) 
+        else:
+            self.model = model
+        self.lr = lr
+        self.gamma = gamma
+        self.actionsize = actionsize
+        self.modelT = dict()
+
+    def discretize(self, obs):
+        """
+        Discretizes the continuous input observation
+
+        @param obs: continuous observation
+        @return: discretized observation  
+        """
+        upper_bounds = [self.env.observation_space.high[0], 5, self.env.observation_space.high[2], math.radians(50)]
+        lower_bounds = [self.env.observation_space.low[0], -5, self.env.observation_space.low[2], -math.radians(50)]
+        ratios = [(obs[i] + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i in range(len(obs))]
+        new_obs = [int(round((self.buckets[i] - 1) * ratios[i])) for i in range(len(obs))]
+        new_obs = [min(self.buckets[i] - 1, max(0, new_obs[i])) for i in range(len(obs))]
+        return tuple(new_obs)
+
+    def qvals(self, states):
+        """
+        Returns the q values for the states.
+
+        @param state: the state
+        
+        @return qvals: the q values for the state for each action. 
+        """
+        qvals = np.zeros((1,2))
+        disc_state = self.discretize(states[0])
+        for i in range(0, 2):
+            if i == 0:
+                qvals[0][i] = self.model[disc_state + (0,)]
+            elif i == 1:
+                qvals[0][i] = self.model[disc_state + (1,)]
+            # elif i == 2:
+            #     qvals[0][i] = self.model[disc_state + (2,)]
+        return qvals
+
+    def td_step(self, state, action, result, next_state, done):
+        """
+        One step TD update to the model
+
+        @param state: the current state
+        @param action: the action
+        @param result: the result of taking the action at the current state
+        @param next_state: the next state after taking the action at the
+            current state
+        @param done: true if episode has terminated, false otherwise
+        @return loss: total loss the at this time step
+        """
+        disc_state = self.discretize(state)
+        q_vals = self.model[disc_state+ (action,)]
+        disc_next = self.discretize(next_state)
+        
+        if done:
+            result = 1.0
+            goal = result
+        else:
+            max_ = max(self.model[disc_next+ (0,)], self.model[disc_next+ (1,)])
+            goal = result
+            goal += self.gamma*max_
+
+        self.model[disc_state + (action,)] = q_vals + self.lr*(goal - q_vals)
+        loss = (q_vals - goal)
+        loss = loss**2
+        return loss
+
+    def save(self, outpath):
+        """
+        saves the model at the specified outpath
+        """
+        torch.save(self.model, outpath)
+
+
+if __name__ == '__main__':
+    args = utils.hyperparameters()
+
+    env = gym.make('CartPole-v1')
+
+    statesize = env.observation_space.shape[0]
+    actionsize = env.action_space.n
+    policy = TabQPolicy(env, buckets=(6, 6, 6, 6), actionsize=actionsize, lr=args.lr, gamma=args.gamma)
+
+    utils.qlearn(env, policy, args)
+
+    torch.save(policy.model, 'tabular.npy')
